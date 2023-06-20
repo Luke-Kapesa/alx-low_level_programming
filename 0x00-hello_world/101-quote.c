186931e5215f83d6a8860b240c6404d0f525f103
Luke Kapesa <lukekapesa@gmail.com>
2023-06-19 19:04:42 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
   *main - Entry point
+  *
   *Return: 1 (success)
   */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (1);
 }
