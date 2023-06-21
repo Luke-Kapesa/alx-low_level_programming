79db2e00b09518d65af61c60636d35519252f812
Luke Kapesa <lukekapesa@gmail.com>
2023-06-21 13:29:46 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - prints the first 98 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	unsigned long int start = 0;
+	unsigned long int nextdigit = 1;
+	unsigned long int sum = 0;
+	unsigned long number = 0;
+
+	while (number < 99)
+	{
+		sum = start + nextdigit;
+		printf("%lu", sum);
+		putchar(',');
+		putchar(' ');
+
+		start = nextdigit;
+		nextdigit = sum;
+
+		number++;
+	}
+	putchar('\n');
+}
