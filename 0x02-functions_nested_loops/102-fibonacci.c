61031e4ad53dc67f8119d74ae448f0fcec59ee0a
Luke Kapesa <lukekapesa@gmail.com>
2023-06-21 12:27:39 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci numbers, starting with 1 and 2
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long int start = 0;
+	long int nextdigit = 1;
+	long int sum = 0;
+	int number = 0;
+
+	while (number < 49)
+	{
+		sum = start + nextdigit;
+		printf("%li, ", sum);
+		start = nextdigit;
+
+		nextdigit = sum;
+
+		number++;
+	}
+	sum = start + nextdigit;
+	printf("%li\n", sum);
+
+	return (0);
+}
